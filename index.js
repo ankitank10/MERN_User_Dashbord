618d511b94c3d047dae5ff5bf90d79d4acc511ac
SAPIENT\agu223 <agupta249@sapient.com>
2018-06-15 00:31:46 +0530

    heroku changes for process

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 const routes = require("./routes/routes");
 routes(app);
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 const port = process.env.PORT || 9999;
 app.listen(port);
